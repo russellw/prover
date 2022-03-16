82a88acbd2dc7c3a8229a434f8162fc187b09256
Russell Wallace <russell.wallace@gmail.com>
2022-03-16 12:38:14 +0000

    fmt

--- src/main/java/olivine/Prover.java
+++ src/main/java/olivine/Prover.java
@@ -1,34 +1,32 @@
 package olivine;
 
-public final class Prover
-{
-    private enum Language {
-        DIMACS,
-        TPTP,
+public final class Prover {
+  private enum Language {
+    DIMACS,
+    TPTP,
+  }
+
+  private static String file;
+
+  private static void setFile(String s) {
+    if (file != null) {
+      System.err.printf("%s: file already specified\n", s);
+      System.exit(1);
     }
+    file = s;
+  }
 
-    private static String file;
-
-    private static void setFile(String s){
-        if(file!=null){
-            System.err.printf("%s: file already specified\n",s);
-            System.exit(1);
-        }
-        file=s;
+  private static void args(String[] args) {
+    for (var i = 0; i < args.length; i++) {
+      var arg = args[i];
+      if (arg.charAt(0) != '-') {
+        setFile(arg);
+        continue;
+      }
     }
+  }
 
-    private static void args(String[] args)  {
-        for (var i = 0; i < args.length; i++) {
-            var arg = args[i];
-            if (arg.charAt(0)!='-'){
-               setFile(arg);
-               continue;
-            }
-        }
-        }
-
-    public static void main( String[] args )
-    {
-        args(args);
-    }
+  public static void main(String[] args) {
+    args(args);
+  }
 }
