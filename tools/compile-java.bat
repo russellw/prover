30a5bc1e73cc423f967db1223337ba2037b71213
Russell Wallace <russell.wallace@gmail.com>
2022-03-26 21:35:27 +0000

    rename

--- tools/compile-java.bat
+++ tools/compile-java.bat
@@ -1,5 +1,5 @@
 del *.class
-C:\jdk-17.0.1\bin\javac -Xlint:unchecked -d . C:\olivine\script\%1.java
+C:\jdk-17.0.1\bin\javac -Xlint:unchecked -d . C:\olivine\tools\%1.java
 if errorlevel 1 goto :eof
 
 C:\jdk-17.0.1\bin\jar -c -e Main -f %1.jar *.class
--- tools/fmt.bat
+++ tools/fmt.bat
@@ -1,4 +1,4 @@
-C:\jdk-17.0.1\bin\java -jar C:\bin\google-java-format-1.15.0-all-deps.jar -i script/*.java
+C:\jdk-17.0.1\bin\java -jar C:\bin\google-java-format-1.15.0-all-deps.jar -i tools/*.java
 C:\jdk-17.0.1\bin\java -jar C:\bin\google-java-format-1.15.0-all-deps.jar -i src/main/java/olivine/*.java
 C:\jdk-17.0.1\bin\java -jar C:\bin\google-java-format-1.15.0-all-deps.jar -i src/test/java/olivine/*.java
 black .
