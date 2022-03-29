ffcd3898ee5dabdb5c8763b61305bf85dad6fcc1
Russell Wallace <russell.wallace@gmail.com>
2022-03-29 15:22:48 +0100

    derivation tests

---
+++ tools/derivation-tests/TypeTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace derivation_tests
+{
+    [TestClass]
+    public class TypeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Type.Bool.Kind, Kind.Bool);
+        }
+    }
+}
