4d13ef5289de5b641256ae6fc3b9697b54f10f3f
Russell Wallace <russell.wallace@gmail.com>
2022-04-01 22:11:27 +0100

    rename

--- etc/fmt.bat
+++ etc/fmt.bat
@@ -1,3 +1,3 @@
 C:\jdk-17.0.1\bin\java -jar C:\bin\google-java-format-1.15.0-all-deps.jar -i src/main/java/olivine/*.java
 C:\jdk-17.0.1\bin\java -jar C:\bin\google-java-format-1.15.0-all-deps.jar -i src/test/java/olivine/*.java
-black tools
+black etc
