86b123193f42231fa8cd6ad6a66fa70acc80a7c9
Russell Wallace <russell.wallace@gmail.com>
2022-04-10 05:57:19 +0100

    compare equations

--- src/main/java/olivine/KnuthBendixOrder.java
+++ src/main/java/olivine/KnuthBendixOrder.java
@@ -11,6 +11,7 @@ public final class KnuthBendixOrder {
   // (because the result could change depending on what the variables are replaced with),
   // so we need four-way comparison. The actual values should be treated as arbitrary
   // by client code; they are chosen to make certain logical operations efficient
+  // TODO: change to enum PartialOrder?
   public static final int EQUALS = 1;
   public static final int LESS = 1 << 1;
   public static final int GREATER = 1 << 2;
@@ -124,10 +125,95 @@ public final class KnuthBendixOrder {
     return compare(a.get(i), b.get(i));
   }
 
-  public int compare(boolean pol0, Equation e0, boolean pol1, Equation e1) {
-    var c = compare(e0.left, e1.left);
+  public int compare(Equation a, Equation b) {
+    switch (compare(a.left, b.left)) {
+      case EQUALS -> {
+        // left terms are equal, so answer depends only on the right
+        return compare(a.right, b.right);
+      }
+      case GREATER -> {
+        // we have a decision on the left, now check the right
+        switch (compare(a.right, b.right)) {
+          case EQUALS, GREATER -> {
+            // result on the right confirms or will not oppose the left
+            return GREATER;
+          }
+          case LESS -> {
+            // opposite result on the right, but now we know which terms are larger, so we can
+            // compare them
+            var c = compare(a.left, b.right);
+
+            // if the larger terms give a result, or are unordered, that's the answer
+            if (c != EQUALS) return c;
+
+            // otherwise, compare the smaller terms
+            return compare(a.right, b.left);
+          }
+          case UNORDERED -> {
+            // no decision on the right, but if a.left is greater than both b terms, then we do have
+            // an answer,
+            // because it doesn't matter what a.right ultimately resolves to
+            if (compare(a.left, b.right) == GREATER) return GREATER;
+          }
+        }
+      }
+      case LESS -> {
+        // we have a decision on the left, now check the right
+        switch (compare(a.right, b.right)) {
+          case EQUALS, LESS -> {
+            // result on the right confirms or will not oppose the left
+            return LESS;
+          }
+          case GREATER -> {
+            // opposite result on the right, but now we know which terms are larger, so we can
+            // compare them
+            var c = compare(a.right, b.left);
+
+            // if the larger terms give a result, or are unordered, that's the answer
+            if (c != EQUALS) return c;
+
+            // otherwise, compare the smaller terms
+            return compare(a.left, b.right);
+          }
+          case UNORDERED -> {
+            // no decision on the right, but if b.left is greater than both a terms, then we do have
+            // an answer,
+            // because it doesn't matter what b.right ultimately resolves to
+            if (compare(a.right, b.left) == LESS) return LESS;
+          }
+        }
+      }
+      case UNORDERED -> {
+        // if the left terms are unordered, it is still possible to have an answer, but the
+        // requirements
+        // are quite specific. if a.right is greater than both b terms, then it doesn't matter what
+        // a.left ultimately resolves to; no matter whether it is greater or less than a.right,
+        // the answer will stand. But if a.right is merely equal to one of the b terms, the answer
+        // could end up depending on whether a.left ultimately resolves to something
+        // greater than both, or less than both, so we have no order. The same applies if b.right
+        // is greater than both a terms
+        switch (compare(a.right, b.right)) {
+          case GREATER -> {
+            if (compare(a.right, b.left) == GREATER) return GREATER;
+          }
+          case LESS -> {
+            if (compare(a.left, b.right) == LESS) return LESS;
+          }
+        }
+      }
+    }
+    return UNORDERED;
+  }
+
+  public int compare(boolean apol, Equation a, boolean bpol, Equation b) {
+    var a0 = a.left;
+    var a1 = a.right;
+    var b0 = b.left;
+    var b1 = b.right;
+
+    var c = compare(a.left, b.left);
     if (c != EQUALS) return c;
-    if (pol0 != pol1) return pol0 ? LESS : GREATER;
-    return compare(e0.right, e1.right);
+    if (apol != bpol) return apol ? LESS : GREATER;
+    return compare(a.right, b.right);
   }
 }
